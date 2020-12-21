bc7446dd7e68a723d2a47982ff1fff69fee0a9f2
vo huy <vovuhuyk21@gmail.com>
2020-12-22 00:57:35 +0700

    TASK-011 : Init Detail Patient view

---
+++ ACV-19/ACV-19/ViewController/DetailPatientViewController/DetailPatientTable/DetailPatientTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  DetailPatientTableViewCell.m
+//  ACV-19
+//
+//  Created by Võ Huy on 11/14/20.
+//  Copyright © 2020 VoHuy. All rights reserved.
+//
+
+#import "DetailPatientTableViewCell.h"
+
+@implementation DetailPatientTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
