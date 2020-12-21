aa54ee9e22bfbec8738e2f579c1716ed3efe190b
vo huy <vovuhuyk21@gmail.com>
2020-12-22 00:45:32 +0700

    TASK-007 : Init Instructions for prevention View

---
+++ ACV-19/ACV-19/ViewController/InstructionsForPreventionController/InstructionsForPreventionViewController.h
@@ -0,0 +1,19 @@
+//
+//  InstructionsForPreventionViewController.h
+//  ACV-19
+//
+//  Created by Võ Huy on 10/22/20.
+//  Copyright © 2020 VoHuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InstructionsForPreventionViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
